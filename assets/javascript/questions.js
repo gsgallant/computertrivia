506e7f6082b90fc22a12a6415e90b7875a0219d3
gsgallant <Greg@nbp-198-22.nbp.ruw.rutgers.edu>
2016-03-03 14:15:29 -0500

    improved game with separate file for the questions array/object and improved function names and comments

---
+++ assets/javascript/questions.js
@@ -0,0 +1,150 @@
+//array of objects.  each element is an object containing a question, an array of choices, correct choice (which element of choices array)
+var allQuestions = [
+
+	{
+			question : "Of which software product category was CP/M a part?",
+			choices : ["spreadsheet","OS","word processing","browser","image editing"],	
+			correctChoice : 1
+	},
+
+	{
+			question : "What company invented the 'floppy disk'?",
+			choices : ["Atari","Hewlett Packard","Intel","Apple","IBM"],	
+			correctChoice : 4
+		},
+	
+	{
+			question : "In what year did Al Gore invent the internet?",
+			choices : ["1980","1983","1987","1989","He didn't"],	
+			correctChoice : 4
+		},
+	
+	{
+			question : "What company manufactured the DEC 1099 Mini-computer?",
+			choices : ["Daimetric Electronics","Dell","Intel","Digital Equipment","None of the above"],	
+			correctChoice : 3
+		},
+	
+	{
+			question : "The first computer 'mouse' was made of what material?",
+			choices : ["silica/carbon","plastic","wood","aluminum","cardboard"],	
+			correctChoice : 2
+		},
+	
+	{
+			question : "Along with whom did Bill Gates found Microsoft?",
+			choices : ["No one","Steve Wozniak","Warren Buffet","Paul Allen","Larry Ellison"],	
+			correctChoice : 3
+		},
+	
+	{
+			question : "Who is regarded as the inventor of the Java programming language?",
+			choices : ["Bill Gates","James Gosling","Steve Jobs","Larry Ellison","Samuel Richards, III"],	
+			correctChoice : 1
+		},
+	
+	{
+			question : "The first patent application for the microprocessor was filed by Intel in what year?",
+			choices : ["Intel wasn't the first to file","1979","1969","1971","1968"],	
+			correctChoice : 0
+		},
+	
+	{
+			question : "What is widely considered the first electronic computer?",
+			choices : ["HAL","ENIAC","SIAGO","BXI583","Brainiac-7"],	
+			correctChoice : 1
+		},
+	
+	{
+			question : "Which of the following is NOT a computer language?",
+			choices : ["MUMPS","PL1","BASIC","PASCAL","LED-EX"],	
+			correctChoice : 4
+		},
+	{	
+			question : "Which Google vice president co-designed TCP/IP and is regarded by some as the father of the Internet?",
+			choices : ["Vint Cerf","Laslow Verring","Steve Jobs","Bill Gates","Albert Einstein"],
+			correctChoice : 0
+		},
+
+	{		question : "Vint Cerf is regarded as the father of the Internet but who is the person that actually invented the World Wide Web?",
+			choices : ["Vint Cerf did that too!","Tim Berners-Lee","Steve Jobs","Bill Gates","Raymond Altervanzt"],
+			correctChoice : 1
+		},
+	{
+			question : "Who is famous for developing the Mosaic web browser at the University of Illinois? Mosaic was later renamed to Netscape Navigator.",
+			choices : ["Larry Ellison","Brian Mosaic","Joshua Redman","Alan Sikes","Marc Andreessen"],
+			correctChoice : 4
+		},
+	{		
+			question : "How many bits are in a byte?",
+			choices : ["4","2","10","8","3"],
+			correctChoice : 3
+		},
+	{
+			question : "What was the first OS for Personal Computers?",
+			choices : ["PCDOS","CP/M","Windows 95","Unix","ZX19"],
+			correctChoice : 1
+		},
+	{
+			question : "What chess-playing computer, developed by IBM, defeated world champion Garry Kasparov in 1997?",
+			choices : ["ENIAC","HAL","RAMBO-9","SQQKING-920","DEEP BLUE"],
+			correctChoice : 4
+
+		},
+	{
+			
+			question : "Who said this: Taking LSD was a profound experience, one of the most important things in my life. LSD shows you that there’s﻿ another side to the coin, and you can’t remember it when it wears off, but you know it. It reinforced my sense of what was important—creating great things instead of making money, putting things back into the stream of history and of human consciousness as much as I could.",
+			choices : ["Albert Einstein","Steve Jobs","Donald Trump","Hilary Clinton","Barack Obama"],
+			correctChoice : 1
+	},
+
+
+	{
+			question : "What is the ERROR number on the Internet for: Site Access Forbidden/Password Protected?",
+			choices : ["1212","403","448","72","9872-012"],
+			correctChoice : 1
+
+		},
+	{
+			question : "What is the Binary Equivalent to 29?",
+			choices : ["11101","01011","01111","1001000","1100101"],
+			correctChoice : 0
+	},
+
+	{	
+			question : "This is an example of what computer language:   loop      mov     al,[si]    ; Load AL from [src]  ?",
+			choices : ["Pascal","BASIC","COBOL","PL1","Assembly"],
+			correctChoice : 4
+		},
+	{		
+			question : "Who invented the data punch card that was used for early computer programming and later became known as the 'IBM Card'?",
+			choices : ["Albert Einstein","Alexander Graham Bell","Herman Hollerith","Nicholaus Copernicus","Leonardo DaVinci"],
+			correctChoice : 2
+
+	},
+
+	{
+			question : "Who invented the PACMAN video game?",
+			choices : ["Steve Wozniak","Steve Balmer","Steve Jobs","Toru Iwatani","Leonard Nimoy"],
+			correctChoice : 3
+	},
+	
+	{		question : "Who founded the NeXT company?",
+			choices : ["Steve Jobs","Laslow Safron","Steve Wozniak","James T. Kirk","John Coltrane"],
+			correctChoice : 0
+	},
+	
+	{
+			question : "Who is widely regarded as the greatest alto saxophone player of all-time?",
+			choices : ["John Coltrane","This is not computer trivia!!!","Charlie 'Bird' Parker","Kenny G","Cannonball Adderley"],
+			correctChoice : 2
+	},
+
+	{
+			question : "In what year did Steve Jobs announce that Apple would transition to Intel microprocessors?",
+			choices : ["2001","2002","2005","1998","None of the above"],
+			correctChoice : 2
+
+	}		
+
+];
